5d288aec9d61732312115b3139ff6f714ae6dc98
pear96 <bhaeun1105@gmail.com>
2023-04-01 18:31:27 +0900

    [Algorithm] 0401 연습 폴더 업로드

---
+++ Algorithm/src/Practice/TestBinary.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class TestBinary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A = 1;
+//		System.out.println("A값 :" + A);
+//		System.out.println("A 이진수 값 :" + Integer.toBinaryString(A));
+//		System.out.println("A 1번 오른쪽으로 밀면? : " + Integer.toBinaryString(A >> 1));
+//		System.out.println("A 10번 왼쪽으로 밀면? : " + Integer.toBinaryString(A << 10));
+//		System.out.println("A 10번 왼쪽으로 밀고 1 빼면? : " + Integer.toBinaryString((A << 10) - 1));
+		int B = (int) (Math.pow(2, 10))-1;
+		System.out.println(B);
+		System.out.println(Integer.toBinaryString(B));
+//		System.out.println(Integer.bitCount(B));
+	}
+
+}
---
+++ Algorithm/src/Practice/TestOperator.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class TestOperator {
+	
+	public static void main(String[] args) {
+		float a = 3;
+		System.out.print(a / 2);
+	}
+
+}
