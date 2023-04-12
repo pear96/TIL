a1ef5e2b4fe1990b17b12193d83cb272d14d6db2
pear96 <bhaeun1105@gmail.com>
2023-04-12 11:33:43 +0900

    [Algorithm] 0412 백준 1018 체스판 다시 칠하기 개선
    - BFS를 활용하던 풀이에서 그냥 2차원 배열 판별로 줄였다.

--- Algorithm/src/baekjoon/체스판다시칠하기/HE1018.java
+++ Algorithm/src/baekjoon/체스판다시칠하기/HE1018.java
@@ -4,79 +4,58 @@
  * 1. N*M 크기의 보드가 있다.
  * 2. 이 보드를 잘라 8*8의 체스판을 만든다.
  * 3. 맨 왼쪽 위가 검은색, 흰색인 두가지 경우가 있다.
- * [해결 방법]
+ * [BFS 해결 방법] : 시간 92ms, 메모리 12120KB
  * 1. 흰색, 검은색 시작으로 M*N 2차원 배열을 2개 만든다.
  * 2. 체스판 확인을 위해 (M-8) * (N-8) 개의 시작 위치를 순회한다.
  * 3. 각 시작위치마다 2개의 종류에 대해 얼마나 다른지 개수를 센다.
- * 4. 두 개수중 더 적은 것을 정답으로 업데이트 한다. 
+ * 4. 두 개수중 더 적은 것을 정답으로 업데이트 한다.
+ * [2차원 배열] : 시간 84ms, 메모리 11720KB
+ * 1. 흰색, 검은색 중 하나를 골라 얼마나 일치하는지 확인한다.
+ * 2. 8*8에서 일치하는 수를 뺀 경우가 반대의 색으로 시작했을 때 일치한 정사각형의 갯수이다.
+ * 3. 따라서 둘 중 더 많이 일치한 수를 고르고, 다시 64에서 빼서 다시 칠해야하는 정사각형의 개수를 업데이트한다.
  * */
 package baekjoon.체스판다시칠하기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class HE1018 {
-	static int N;
-	static int M;
-	static int[] dr = {0, 1};
-	static int[] dc = {1, 0};
 	
-	static class Pos {
-		int r;
-		int c;
-		
-		public Pos(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
+	static int answer = Integer.MAX_VALUE;
+	static boolean[][] board;
 	
-	static boolean inRange(int r, int c) {
-		if (0 <= r && r < N && 0 <= c && c < M) return true;
-		return false;
-	}
-	
-	static boolean[][] fill(boolean start) {
-		boolean[][] arr = new boolean[N][M];
-		boolean[][] visited = new boolean[N][M];
-		LinkedList<Pos> queue = new LinkedList<>();
+	static void check(int startR, int startC) {
+		boolean color = true; // 흰색으로 시작
+		int cnt = 0;
 		
-		arr[0][0] = start;
-		queue.add(new Pos(0, 0));
-		visited[0][0] = true;
-		
-		while(!queue.isEmpty()) {
-			Pos now = queue.poll();
-			for(int d = 0; d < 2; d++) {
-				int nextR = now.r + dr[d];
-				int nextC = now.c + dc[d];
-				if (inRange(nextR, nextC) && !visited[nextR][nextC]) {
-					arr[nextR][nextC] = !arr[now.r][now.c];
-					visited[nextR][nextC] = true;
-					queue.add(new Pos(nextR, nextC));
-				}
+		for (int r = 0; r < 8; r++) {
+			for (int c = 0; c < 8; c++) {
+//				System.out.println(r +","+c+" " +board[startR+r][startC+c] + " " + color);
+				if (board[startR+r][startC+c] == color) cnt++;
+				color = !color;
 			}
-			
+			color = !color; // 다음 줄은 다른 색으로 시작해야해서 홀수로 바꿔야됨
 		}
-		return arr;
+		// cnt : 흰색으로 시작했을 때 제대로 칠해진 정사각형의 개수
+		// 64 - cnt : 검은색으로 시작했을 때 제대로 칠해진 정사각형의 개수
+		// 둘 중 더 큰 것을 고르고, 다시 칠해야하는 정사각형의 개수를 구해야하니깐 64에서 뺀다.
+		cnt = cnt > 64 - cnt ? cnt : 64 - cnt;
+		answer = Math.min(answer, 64 - cnt);
+		
 	}
 
 	public static void main(String[] args) throws IOException {
-		int answer = Integer.MAX_VALUE;
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		boolean[][] black = fill(false);
-		boolean[][] white = fill(true);
+		board = new boolean[N][M];
 		
-		boolean[][] board = new boolean[N][M];
-		
+		// B면 false, W면 true
 		for(int i = 0; i < N; i++) {
 			char[] input = br.readLine().toCharArray();
 			for (int j = 0; j < M; j++) {
@@ -85,19 +64,10 @@ public class HE1018 {
 			}
 		}
 		
+		// 계산
 		for(int startR = 0; startR < N-7; startR++) {
 			for(int startC = 0; startC < M-7; startC++) {
-				int blackCnt = 0;
-				int whiteCnt = 0;
-				
-				for (int r = 0; r < 8; r++) {
-					for (int c = 0; c < 8; c++) {
-						if (black[startR+r][startC+c] != board[startR+r][startC+c]) blackCnt += 1;
-						if (white[startR+r][startC+c] != board[startR+r][startC+c]) whiteCnt += 1;
-					}
-				}
-				int minFix = Math.min(whiteCnt, blackCnt);
-				answer = Math.min(answer, minFix);
+				check(startR, startC);
 			}
 		}
 		System.out.println(answer);
