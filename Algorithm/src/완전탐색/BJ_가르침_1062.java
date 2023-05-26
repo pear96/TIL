ea13e784a4eeff2f6fa1004f2c24247bf355dcee
pear96 <bhaeun1105@gmail.com>
2023-05-26 22:28:49 +0900

    [Algorithm] 백준 1062 가르침 2트
    - 속상하다.. 다시 풀자
    1. K==26 return 안써서 틀림
    2. alphabet 빨리 넘어가기 위해 idx도 backtracking 함수에 파라미터로 전달해야함.

--- Algorithm/src/완전탐색/BJ_가르침_1062.java
+++ Algorithm/src/완전탐색/BJ_가르침_1062.java
@@ -3,83 +3,101 @@ package 완전탐색;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BJ_가르침_1062 {
-	static int N, K;
-	static String[] word;
-	static boolean[] visited;
 	static int answer = 0;
+	static int N, K;
+	static String[] words;
+	static boolean[] learned;
 	
-	static void backtracking(int alpha, int cnt) {
-		// 필수로 사용해야하는 알파벳 제외한 개수를 모두 채웠다.
-		if (cnt == K - 5) {
-			// 몇개의 단어를 읽을 수 있는가?
-			int count = 0;
-			for (int i = 0; i < N; i++) {
-				boolean readable = true;
-				for(int c = 0; c < word[i].length(); c++) {
-					if (!visited[word[i].charAt(c) - 'a']) {
-						readable = false;
+	/**
+	 * 알파벳 조합을 만들어 몇개의 단어를 읽을 수 있는지 확인하는 함수
+	 * @param cnt : 몇 개의 알파벳을 배웠는가
+	 * 
+	 * @param alpha : 몇 번째 알파벳 까지 봤는가
+	 */
+	static void makeAlphaComb(int cnt, int alpha) {
+		// 정해진 개수만큼 다 배웠다. (acint 제외)
+		// 1번째 백트랙킹
+		if (cnt == K-5) {
+			// 이번 알파벳 조합으로 읽을 수 있는 단어의 수
+			int wordsCnt = 0;
+			
+			for (String word : words) {
+				// 해당 단어를 읽을 수 있는가?
+				boolean possible = true;
+				
+				for(int c = 0; c < word.length(); c++) {
+					if (!learned[word.charAt(c) - 'a']) {
+						possible = false;
 						break;
 					}
 				}
-				if (readable) {
-					count++;
-				}
+				
+				if (possible) wordsCnt++;
 			}
-			answer = Math.max(answer, count);
+			answer = Math.max(answer, wordsCnt);
 			return;
 		}
 		
-		
-		
-		for (int i = alpha; i < 26; i++) {
-			// 사용하지 않은 알파벳이라면
-			if (visited[i] == false) {
-				visited[i] = true;
-				// i 알파벳 사용하기로 함. 사용한 알파벳 수 +1
-				backtracking(i, cnt+1);
-				// 사용 해제 후 다른 알파벳 보러 감
-				visited[i] = false;
-			}
+		// 알파벳 조합을 만들어본다.
+		// 2번째 백트랙킹 : 이전 알파벳으로 돌아갈 이유가 있을까? 
+		// 아니, 우리는 조합을 보고있잖아. '순서'는 중요하지 않아.
+		for(int idx = alpha; idx < 26; idx++) {
+			// 해당 알파벳을 이미 배웠다면 넘어간다.
+			if (learned[idx]) continue;
+			
+			// 안 배웠다면 배워본다.
+			learned[idx] = true;
+			// 이미 learned[idx]에 체크한 본인을 넘어갈 수 없어 아쉽겠지만
+			// idx+1로 하면 idx=25일때 for문이 안돌아서 z를 못 본다.
+			// 그렇다고 idx < 27로 하면 Index 에러 나겠지?^^
+			makeAlphaComb(cnt+1, idx);
+			// 다른 조합을 위해 false 처리를 진행한다.
+			learned[idx] = false;
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		N = Integer.parseInt(input[0]);
-		K = Integer.parseInt(input[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		word = new String[N];
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 		
-		// 접두사 'anta'와 접미사 'tica' 제거 후 저장 
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			str.substring(4, str.length()-4);
-			word[i] = str;
-		}
-		
-		// 특수 상황
+		// 특수한 경우 미리 처리
 		if (K < 5) {
-			System.out.println("0");
+			// 기본으로 필요한 알파벳 acint 때문에 5개 미만이면 아무것도 읽을 수 없음
+			System.out.println(0);
 			return;
 		}
 		if (K == 26) {
+			// 모든 알파벳을 배울 수 있다면 전부 읽을 수 있음
 			System.out.println(N);
 			return;
 		}
 		
+		// 입력된 N개의 단어들
+		words = new String[N];
 		
-		// 알파벳 사용 체크
-		visited = new boolean[26];
-		visited['a' - 'a'] = true;
-		visited['n' - 'a'] = true;
-		visited['t' - 'a'] = true;
-		visited['i' - 'a'] = true;
-		visited['c' - 'a'] = true;
+		for(int i = 0; i < N; i++) {
+			String input = br.readLine();
+			// 3번째 백트랙킹 : 'anta' 와 'tica' 제거
+			words[i] = input.substring(4, input.length()-4);
+		}
 		
-		backtracking(0, 0);
+		// 해당 알파벳을 배웠는지 표시
+		learned = new boolean[26];
+		// a c i n t
+		learned['a' - 'a'] = true;
+		learned['c' - 'a'] = true;
+		learned['i' - 'a'] = true;
+		learned['n' - 'a'] = true;
+		learned['t' - 'a'] = true;
+		
+		makeAlphaComb(0, 0);
+		
 		System.out.println(answer);
 	}
 
