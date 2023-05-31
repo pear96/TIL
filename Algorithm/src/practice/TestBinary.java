f726da89c3298ec1bc8dbaac5b15037a86e36f10
pear96 <bhaeun1105@gmail.com>
2023-05-31 15:18:15 +0900

    [Algorithm] 0531 백준 1062
    - 복습겸 비트마스킹 활용
    - 어이없는 실수를 방지하기 위해선 명확한 변수명이 필수
    - 백트랙킹 + 비트마스킹

--- Algorithm/src/practice/TestBinary.java
+++ Algorithm/src/practice/TestBinary.java
@@ -3,24 +3,23 @@ package practice;
 public class TestBinary {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int A = 1;
-//		System.out.println("A값 :" + A);
-//		System.out.println("A 이진수 값 :" + Integer.toBinaryString(A));
-//		System.out.println("A 1번 오른쪽으로 밀면? : " + Integer.toBinaryString(A >> 1));
-//		System.out.println("A 10번 왼쪽으로 밀면? : " + Integer.toBinaryString(A << 10));
-//		System.out.println("A 10번 왼쪽으로 밀고 1 빼면? : " + Integer.toBinaryString((A << 10) - 1));
+		System.out.println("A값 :" + A); // 1
+		System.out.println("A 이진수 값 :" + Integer.toBinaryString(A)); // 1
+		System.out.println("A 1번 오른쪽으로 밀면? : " + Integer.toBinaryString(A >> 1)); // 0
+		System.out.println("A 10번 왼쪽으로 밀면? : " + Integer.toBinaryString(A << 10)); // 10000000000
+		System.out.println("A 10번 왼쪽으로 밀고 1 빼면? : " + Integer.toBinaryString((A << 10) - 1)); // 1111111111
 		int B = (int) (Math.pow(2, 10))-1;
-		System.out.println(B);
-		System.out.println(Integer.toBinaryString(B));
-//		System.out.println(Integer.bitCount(B));
+		System.out.println(B); // 1023
+		System.out.println(Integer.toBinaryString(B)); // 1111111111
+		System.out.println(Integer.bitCount(B)); // 10
 		
-		System.out.println(1 << 0);
-		System.out.println(1 << 1);
-		System.out.println(1 << 2);
-		System.out.println(1 >> 2);
-		System.out.println(1 >> 1);
-		System.out.println(1 >> 0);
+		System.out.println(1 << 0); // 1
+		System.out.println(1 << 1); // 2
+		System.out.println(1 << 2); // 4
+		System.out.println(1 >> 2); // 0
+		System.out.println(1 >> 1); // 0
+		System.out.println(1 >> 0); // 1
 	}
 
 }
