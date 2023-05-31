fef16c7de6cc8003720a2c0ce3094153070ccc88
pear96 <bhaeun1105@gmail.com>
2023-05-31 16:26:13 +0900

    [Algorithm] 0531 프로그래머스 등산코스 정하기
    - 복습

--- Algorithm/src/Dijkstra/PG_등산코스정하기_118669.java
+++ Algorithm/src/Dijkstra/PG_등산코스정하기_118669.java
@@ -1,26 +1,10 @@
-package Dijkstra;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 
 class Solution {
-    PriorityQueue<Node> pq;
-    ArrayList<ArrayList<Node>> adj;
-    
-    int maxIntensity = Integer.MAX_VALUE;
-    int maxNode = Integer.MAX_VALUE;
-    
-    // gate를 따로 확인하지 않는 이유는, 출입구는 한번에 0으로 설정하고 볼 것이기 때문이다.
-    HashSet<Integer> peaks = new HashSet<>();
-    
-    // i번째 노드의 값 = i번째 노드까지 오면서 본 최대 intensity값
-    // 대신 pq로 거리가 가까운 부분부터 지나오기 때문에 간선 중 최대값 X
-    // 최대값이 가장 작은 등산로에서의 i번째 노드까지 보면서 찾은 최대값
-    int[] intensity;
-    
-    class Node implements Comparable<Node> {
+    class Node implements Comparable<Node>{
         int num;
         int cost;
         
@@ -30,43 +14,19 @@ class Solution {
         }
         
         @Override
-        public int compareTo(Node node) {
-            return this.num - node.num;
+        public int compareTo(Node n) {
+            return this.num - n.num;
         }
     }
     
-    void dijkstra() {
-
-        while(!pq.isEmpty()) {
-            Node now = pq.poll();
-            
-            for(Node other : adj.get(now.num)) {
-                // now와 연결된 other 노드들을 보면서
-                // other 노드까지의 최대 intensity가, now까지의 최대 intensity값 또는 other의 가중치보다 크다면
-                // 바꿔준다.. ㅜㅜ 이해 앙됨
-                if (intensity[other.num] > Math.max(intensity[now.num], other.cost)) {
-                    intensity[other.num] = Math.max(intensity[now.num], other.cost);
-                    // 만약 산봉우리가 아니라면, 
-                    if (!peaks.contains(other.num)) pq.add(other);
-                }
-            }
-        }
-    }
-
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        // 출입구 A에서 출발하여 index번 지점까지 올 때 가능한 최소 intensity
-        intensity = new int[n+1];
-        // intensity 전부 MAX값
-        Arrays.fill(intensity, Integer.MAX_VALUE);
+        ArrayList<ArrayList<Node>> adj;
+        HashSet<Integer> peaks = new HashSet<>();
+        int maxIntensity = Integer.MAX_VALUE;
+        int maxNode = Integer.MAX_VALUE;
         
-        pq = new PriorityQueue<>();
+        // 연결리스트 생성
         adj = new ArrayList<>();
-        
-        // 출입구 다 넣어놓고, 출입구는 intensity 0임
-        for(int gate : gates) {
-            pq.add(new Node(gate, 0));
-            intensity[gate] = 0;
-        }
         
         for(int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
@@ -76,28 +36,61 @@ class Solution {
             int a = path[0];
             int b = path[1];
             int c = path[2];
+            
             // 양방향 연결
             adj.get(a).add(new Node(b, c));
             adj.get(b).add(new Node(a, c));
         }
         
-        // 산봉우리 set
+        // i번째 노드까지 본 intensity중 최대값
+        int[] intensity = new int[n+1];
+        Arrays.fill(intensity, Integer.MAX_VALUE);
+        
+        // 산봉우리 Set -> 다익스트라 볼 때 산봉우리는 pq에 추가 안하려고
         for(int summit : summits) {
             peaks.add(summit);
         }
         
-
-        dijkstra();
+        // 다익스트라 시작
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         
-        // peaks에서만 고른다.
-        Arrays.sort(summits);
+        // PQ에 출입구 넣기 + 출입구는 시작이므로 최대 intensity가 없음
+        for(int gate : gates) {
+            pq.add(new Node(gate, 0));
+            intensity[gate] = 0;
+        }
+        
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+            
+            // 지금 나보다 같거나 크면, 그 쪽으로 가볼 이유가 없음
+            if (intensity[now.num] < now.cost) continue;
+            intensity[now.num] = now.cost;
+            
+            // 나랑 연결된 노드들 중에서
+            for(Node other : adj.get(now.num)) {
+                // now까지의 최대 intensity랑, 내 cost랑 비교해서 더 큰 값
+                // 지금 pq라서 cost가 작은 것 부터 보면서 지나오고 있다.
+                // 둘 중 큰 값을 찾는 이유는, 최대 intensity를 찾아야 하기 때문
+                // pq를 쓰는 이유는, 그 최대 intensity의 값이 가장 작길 원하기 때문
+                int newIntensity = Math.max(intensity[now.num], other.cost);
+                if (newIntensity < intensity[other.num]) {
+                    intensity[other.num] = newIntensity;
+                    // 산봉우리는 못 간다.
+                    if (!peaks.contains(other.num)) pq.add(new Node(other.num, intensity[other.num]));
+                }
+            }
+        }        
+        // 다익스트라 끝났으면, 산봉우리만 보면서 '최소' intensity값을 찾는다. (그게 최대 intensity가 최소인 등산경로)
         for(int summit : summits) {
             if (intensity[summit] < maxIntensity) {
                 maxIntensity = intensity[summit];
                 maxNode = summit;
+            } else if (intensity[summit] == maxIntensity && maxNode > summit) {
+                maxNode = summit;
             }
         }
-        // System.out.println(Arrays.toString(intensity));
-        return new int[]{maxNode, maxIntensity};
+        
+        return new int[] {maxNode, maxIntensity};
     }
 }
